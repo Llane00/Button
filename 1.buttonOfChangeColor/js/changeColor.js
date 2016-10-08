e1fc82c404a04c9cc1a3b8bc2c4cea936060c223
Llane00 <bluetinestyle@126.com>
2016-10-08 09:22:22 +0800

    first commit

---
+++ 1.buttonOfChangeColor/js/changeColor.js
@@ -0,0 +1,10 @@
+﻿// change container's background-color into button's background-color
+function changeColor(whichButton) {
+	if(!document.getElementById) return false;
+	if(!document.getElementById("container")) return false;
+	// step1 get button's background-color
+	var button_backgroundColor = whichButton.style.backgroundColor;
+	// step2 replace container's background-color with button)
+	var container = document.getElementById("container");
+	container.style.backgroundColor = button_backgroundColor ;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+##Button Demo.
+
+[1.buttonOfChangeColor demo.](http://htmlpreview.github.io/?https://github.com/Llane00/Button/blob/master/1.buttonOfChangeColor/index.html)
+
+[2.responsiveSlide demo.](http://htmlpreview.github.io/?https://github.com/Llane00/Button/blob/master/2.responsiveSlide/index.html)
+
